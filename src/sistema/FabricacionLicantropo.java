5f9ca3c9cc6f784330590c059dd501dbb507f30a
mimi tee moc <mimit@DESKTOP-5GHP32N.com>
2022-04-26 00:11:43 +0200

    fabricado el licantropo

--- src/sistema/FabricacionLicantropo.java
+++ src/sistema/FabricacionLicantropo.java
@@ -4,10 +4,50 @@
  */
 package sistema;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mimit
  */
-public class FabricacionLicantropo {
-    
+public class FabricacionLicantropo extends Fabrica{
+    @Override
+    public Personaje crearPersonaje(){
+        Licantropo licantropo = new Licantropo();
+        licantropo.anadirNombre();
+        licantropo.construirHabilidad();
+        String opc = null;
+        do{
+          licantropo.anadirArma();
+          System.out.println("¿desea anadir otra arma? -si o no-");
+          Scanner sc = new Scanner(System.in);
+          opc = sc.next();
+        }while(opc == "si");
+        do{
+          licantropo.anadirArmadura();
+          System.out.println("¿desea anadir otra armadura? -si o no-");
+          Scanner sc = new Scanner(System.in);
+          opc = sc.next();
+        }while(opc == "si");
+        licantropo.getRabia();
+        licantropo.anadirOro();
+        licantropo.anadirPoder();
+        licantropo.anadirSalud();
+        do{
+          licantropo.construirFortaleza();
+          System.out.println("¿desea anadir otra fortaleza? -si o no-");
+          Scanner sc = new Scanner(System.in);
+          opc = sc.next();
+        }while(opc == "si");
+        do{
+          licantropo.construirDebilidad();
+          System.out.println("¿desea anadir otra debilidad? -si o no-");
+          Scanner sc = new Scanner(System.in);
+          opc = sc.next();
+        }while(opc == "si");
+        licantropo.construirGhoul();
+        licantropo.construirDemonio();
+        licantropo.construirHumano();
+        return licantropo;
+    }
 }
