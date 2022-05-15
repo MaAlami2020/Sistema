087913a71023b40b208de709caff7fa29fac035c
mimit <mimit@DESKTOP-5GHP32N.com>
2022-05-15 09:06:13 +0200

    corregi errores de los tests de la clase Armadura

--- test/sistema/ArmaduraTest.java
+++ test/sistema/ArmaduraTest.java
@@ -38,15 +38,15 @@ public class ArmaduraTest {
 
     /**
      * Test of setModificadorAtaque method, of class Armadura.
-     * test para comprobar el siguiente valor que sobrepasa el valor maximo de modificador al ataque
+     * test para comprobar el siguiente valor del limite superior del rango [1-3]
+     * sobrepasa el valor maximo de modificador al ataque
      */
     @Test
     public void testSetModificadorAtaque(){
-        System.out.println("setModificadorAtaque");
         Armadura armadura = new Armadura();
         try {
              armadura.setModificadorAtaque(4);
-             assertEquals(4,armadura.getModificadorAtaque());
+             fail("no se sobrepasó el límite de modificador de ataque");
              System.out.println("CP1 incorrecto");
         } catch(Exception ex){
              Exception e = null;
@@ -57,7 +57,7 @@ public class ArmaduraTest {
         }
     }
     /**
-     * test para comprobar el valor minimo del rango del modificador al ataque.
+     * test para probar el valor minimo del rango [1-3] del modificador al ataque
      * El valor minimo es 0, lo que significa que el armadura no tiene modificador al ataque
      */
     @Test
@@ -68,14 +68,12 @@ public class ArmaduraTest {
              assertEquals(0,a1.getModificadorAtaque());
              System.out.println("CP2 incorrecto");
         } catch(Exception ex) {
-            Exception e = null;
-            e = ex;
-            assertEquals(e.getMessage(),null);
+            fail("se sobrepasó el límite de modificador de ataque");
             System.out.println("CP2 correcto");
         }
     }
     /**
-     * test para comprobar el valor limite del rango [1-3] del modificador al ataque
+     * test para comprobar un valor limite del rango [1-3] del modificador al ataque
      */
     @Test
     public void setModificadorAtaque2(){
@@ -85,21 +83,20 @@ public class ArmaduraTest {
              assertEquals(3,a2.getModificadorAtaque());
              System.out.println("CP3 incorrecto");
         } catch(Exception ex) {
-            Exception e = null;
-            e = ex;
-            assertEquals(e.getMessage(),null);
+            fail("se sobrepasó el límite de modificador de ataque");
             System.out.println("CP3 correcto");
         }
     }
     /**
-     * test para comprobar que el valor anterior que esta fuera del valor minimo de modificador al ataque
+     * test para comprobar que el valor anterior del limite inferior del rango [1-3] 
+     * esta fuera del valor minimo de modificador al ataque
      */
     @Test
     public void setModificadorAtaque3(){
         Armadura a3 = new Armadura();
         try {
              a3.setModificadorAtaque(-1);
-             assertEquals(-1,a3.getModificadorAtaque());
+             fail("no se sobrepasó el límite de modificador de ataque");
              System.out.println("CP4 incorrecto");
         } catch(Exception ex) {
             Exception e = null;
@@ -109,7 +106,7 @@ public class ArmaduraTest {
         }
     }
     /**
-     * test para comprobar que el valor del rango [1-3] del modificador al ataque sin incluir los extremos
+     * test para comprobar un valor del rango [1-3] del modificador al ataque sin incluir los extremos
      */
     @Test
     public void setModificadorAtaque4(){
@@ -119,14 +116,12 @@ public class ArmaduraTest {
              assertEquals(2,a4.getModificadorAtaque());
              System.out.println("CP5 incorrecto");
         } catch(Exception ex) {
-            Exception e = null;
-            e = ex;
-            assertEquals(e.getMessage(),null);
+            fail("se sobrepasó el límite de modificador de ataque");
             System.out.println("CP5 correcto");
         }
     }
     /**
-     * test para comprobar que un valor que esta fuera del limite superior de modificador al ataque
+     * test para probar un valor que esta fuera del limite superior del rango [1-3] de modificador al ataque
      * y no es el siguiente valor
      */
     @Test
@@ -134,7 +129,7 @@ public class ArmaduraTest {
         Armadura a5 = new Armadura();
         try {
              a5.setModificadorAtaque(8);
-             assertEquals(8,a5.getModificadorAtaque());
+             fail("no se sobrepasó el límite de modificador de ataque");
              System.out.println("CP5 incorrecto");
         } catch(Exception ex) {
             Exception e = null;
@@ -144,7 +139,7 @@ public class ArmaduraTest {
         }
     }
     /**
-     * test para comprobar que un valor que esta fuera del limite inferior de modificador al ataque
+     * test para comprobar que un valor que esta fuera del limite inferior del rango [1-3] de modificador al ataque
      * y no es el anterior valor
      */
     @Test
@@ -152,7 +147,7 @@ public class ArmaduraTest {
         Armadura a6 = new Armadura();
         try {
              a6.setModificadorAtaque(-5);
-             assertEquals(-5,a6.getModificadorAtaque());
+             fail("no se sobrepasó el límite de modificador de ataque");
              System.out.println("CP6 incorrecto");
         } catch(Exception ex) {
             Exception e = null;
@@ -164,25 +159,25 @@ public class ArmaduraTest {
     
     /**
      * Test of setModificadorDefensa method, of class Armadura.
-     * test para comprobar el siguiente valor que sobrepasa el valor maximo de modificador a la defensa
+     * test para comprobar que el siguiente valor del limite superior del rango [1-3]
+     * sobrepasa el valor maximo de modificador a la defensa
      */
     @Test
     public void setModificadorDefensa(){
         Armadura a = new Armadura();
         try {
             a.setModificadorDefensa(4);
-            assertEquals(4,a.getModificadorDefensa());
-            System.out.println("CP7 incorrecto");
+            fail("no se sobrepasó el límite de modificador de defensa");
+            System.out.println("CP1 incorrecto");
         } catch(Exception ex) {
             Exception e = null;
             e = ex;
             assertEquals(e.getMessage(),"sobrepasó el límite del modificador de defensa");
-            System.out.println("CP7 correcto");
-            //fail("sobrepasó el límite del modificador de defensa");
+            System.out.println("CP1 correcto");
         }       
     }
     /**
-     * test para comprobar el valor del limite superior del modifcador a la defensa del rango [1-3]
+     * test para probar un valor del limite del rango [1-3] del modifcador a la defensa
      */
     @Test
     public void setModificadorDefensa1(){
@@ -190,16 +185,14 @@ public class ArmaduraTest {
         try {
              a1.setModificadorDefensa(3);
              assertEquals(3,a1.getModificadorDefensa());
-             System.out.println("CP8 incorrecto");
+             System.out.println("CP2 incorrecto");
         } catch(Exception ex) {
-            Exception e = null;
-            e = ex;
-            assertEquals(e.getMessage(),null);
-            System.out.println("CP8 correcto");
+            fail("se sobrepasó el límite de modificador de defensa");
+            System.out.println("CP2 correcto");
         }
     }
     /**
-     * test para comprobar el valor del limite inferior del modifcador a la defensa del rango [1-3]
+     * test para probar el valor del limite inferior del rango [1-3] del modifcador a la defensa
      */
     @Test
     public void setModificadorDefensa2(){
@@ -207,12 +200,10 @@ public class ArmaduraTest {
         try {
              a2.setModificadorDefensa(1);
              assertEquals(1,a2.getModificadorDefensa());
-             System.out.println("CP9 incorrecto");
+             System.out.println("CP3 incorrecto");
         } catch(Exception ex) {
-            Exception e = null;
-            e = ex;
-            assertEquals(e.getMessage(),null);
-            System.out.println("CP9 correcto");
+            fail("se sobrepasó el límite de modificador de defensa");
+            System.out.println("CP3 correcto");
         }
     }
     /**
@@ -224,17 +215,17 @@ public class ArmaduraTest {
         Armadura a3 = new Armadura();
         try {
              a3.setModificadorDefensa(0);
-             assertEquals(0,a3.getModificadorDefensa());
-             System.out.println("CP10 incorrecto");
+             fail("no se sobrepasó el límite de modificador de defensa");
+             System.out.println("CP4 incorrecto");
         } catch(Exception ex) {
             Exception e = null;
             e = ex;
             assertEquals(e.getMessage(),"sobrepasó el límite del modificador de defensa");
-            System.out.println("CP10 correcto");
+            System.out.println("CP4 correcto");
         }
     }
     /**
-     * test para comprobar el valor del rango [1-3] del modifcador a la defensa sin incluir los extremos
+     * test para probar un valor del rango [1-3] del modifcador a la defensa sin incluir los extremos
      */
     @Test
     public void anadirModifDefensa4(){
@@ -242,46 +233,46 @@ public class ArmaduraTest {
         try {
              a4.setModificadorDefensa(2);
              assertEquals(2,a4.getModificadorDefensa());
-             System.out.println("CP11 incorrecto");
+             System.out.println("CP5 incorrecto");
         } catch(Exception ex) {
-            Exception e = null;
-            e = ex;
-            assertEquals(e.getMessage(),null);
-            System.out.println("CP11 correcto");
+            fail("se sobrepasó el límite de modificador de defensa");
+            System.out.println("CP5 correcto");
         }
     }
     /**
-     * test para comprobar que un valor que esta fuera del limite superior de modificador a la defensa y no es el siguiente
+     * test para comprobar que un valor que esta fuera del limite superior del rango [1-3]
+     * de modificador a la defensa y no es el siguiente
      */
     @Test
     public void anadirModifDefensa5(){
         Armadura a5 = new Armadura();
         try {
              a5.setModificadorDefensa(8);
-             assertEquals(8,a5.getModificadorDefensa());
-             System.out.println("CP12 incorrecto");
+             fail("no se sobrepasó el límite de modificador de defensa");
+             System.out.println("CP6 incorrecto");
         } catch(Exception ex) {
             Exception e = null;
             e = ex;
             assertEquals(e.getMessage(),"sobrepasó el límite del modificador de defensa");
-            System.out.println("CP12 correcto");
+            System.out.println("CP6 correcto");
         }
     }
     /**
-     * test para comprobar que un valor que esta fuera del limite inferior de modificador a la defensa y no es el anterior
+     * test para probar que un valor que esta fuera del limite inferior del rango [1-3]
+     * de modificador a la defensa y no es el anterior
      */
     @Test
     public void anadirModifDefensa6(){
         Armadura a6 = new Armadura();
         try {
              a6.setModificadorDefensa(-5);
-             assertEquals(-5,a6.getModificadorDefensa());
-             System.out.println("CP13 incorrecto");
+             fail("no se sobrepasó el límite de modificador de defensa");
+             System.out.println("CP7 incorrecto");
         } catch(Exception ex) {
             Exception e = null;
             e = ex;
             assertEquals(e.getMessage(),"sobrepasó el límite del modificador de defensa");
-            System.out.println("C13 correcto");
+            System.out.println("CP7 correcto");
         }
     }
     
